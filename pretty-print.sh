133eaa3d2cc58cf7ffb3ef8b9ca194044c237136
FlandreDaisuki <vbnm123c@gmail.com>
2022-06-19 14:27:13 +0800

    Make report prettier ❤️

--- pretty-print.sh
+++ pretty-print.sh
@@ -2,9 +2,14 @@
 
 DIRNAME="$(dirname "$0")"
 
-docker-compose -f "$DIRNAME/docker-compose.yml" exec speedtest-monitor jq --raw-output '
-( .timestamp | fromdate | strflocaltime("%F %X") ) as $localtime |
-( (.download.bandwidth * 8 / 1000 | round) / 1000 ) as $download_Mbps |
-(   (.upload.bandwidth * 8 / 1000 | round) / 1000 ) as $upload_Mbps |
-"[\($localtime)]\tdownload: \($download_Mbps)Mbps\tupload: \($upload_Mbps)Mbps"
+docker-compose -f "$DIRNAME/docker-compose.yml" exec speedtest-monitor jq --slurp --raw-output '
+def down_avg: ( map(.download.bandwidth) | add ) / length;
+def up_avg: ( map(.upload.bandwidth) | add ) / length;
+def to_fixed(n): tostring | capture("(?<int>\\d+)?(?<frac>[.]\\d+)?") | "\(.int).\(((.frac // "0") + "0" * n)[1:n+1])";
+def to_mbps: "\(. * 8 / 1000000 | to_fixed(2)) Mbps";
+def to_localtime: fromdate | strflocaltime("%F %X");
+
+map("[\(.timestamp | to_localtime)]\tdown:\t\(.download.bandwidth | to_mbps)\tup:\t\(.upload.bandwidth | to_mbps)")[],
+"\t\t\t\( "=" * 44 )",
+"\t\t\tavg:\t\(down_avg | to_mbps)\tavg:\t\(up_avg | to_mbps)"
 ' /root/result.jsonl
